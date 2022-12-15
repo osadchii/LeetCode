8b365d3753ae8c7f4bcf3d8a179dc3e31f789155
a.osadchii <roflandcom@gmail.com>
2022-12-15 20:39:15 +0200

    Length of Last Word

---
+++ Length of Last Word/Solution/Program.cs
@@ -0,0 +1,26 @@
+﻿var testCases = new Dictionary<string, int>()
+{
+    ["Hello World"] = 5,
+    ["   fly me   to   the moon  "] = 4,
+    ["luffy is still joyboy"] = 6
+};
+
+var solution = new Solution();
+
+foreach (var testCase in testCases)
+{
+    var result = solution.LengthOfLastWord(testCase.Key);
+    if (result != testCase.Value)
+    {
+        throw new Exception("Wrong result");
+    }
+}
+
+public class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        var words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var lastWord = words.Last();
+        return lastWord.Length;
+    }
+}
---
+++ Length of Last Word/readme.md
@@ -0,0 +1,7 @@
+**Length of Last Word**
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Problem: https://leetcode.com/problems/length-of-last-word/
